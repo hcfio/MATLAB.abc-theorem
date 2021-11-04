eaac5cc5cbd50e1402c3b162279fe5b41c5261b7
Hiroyuki Chihara <77265300+hiroyuki-chihara@users.noreply.github.com>
2021-11-04 23:38:03 +0900

    Add files via upload

---
+++ abc.m
@@ -0,0 +1,29 @@
+% Examples of abc theorem
+
+kappa=1.4;
+Q=[1 2 3];
+
+for a=1:1000
+    for b=a+1:100000
+        c=a+b;
+        X=factor(a*b*c);
+        L=length(X);
+        rad=X(1,1);
+        if [gcd(a,b) gcd(a,c) gcd(b,c)]==[1 1 1]
+            rad=X(1,1);
+            for i=2:L
+                if X(1,i)==X(1,i-1)
+                    rad=rad*1;
+                else
+                    rad=rad*X(1,i);
+                end
+            end
+            if (c >= rad^kappa)
+                q=[a b c]
+                Q=[Q;q];
+            end
+        end
+    end
+end
+
+writematrix(Q,'abc11.csv')
